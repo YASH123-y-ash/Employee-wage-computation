875c7effa7fe3697ae807b3e9f390cfcedbd5415
Yash-y-ash <wardhan696@gmail.com>
2021-03-23 13:44:13 +0530

    Added:welcome message

---
+++ src/Emp_Wage_Problem/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package Emp_Wage_Problem;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to employee wage computation");
+
+	}
+
+}
