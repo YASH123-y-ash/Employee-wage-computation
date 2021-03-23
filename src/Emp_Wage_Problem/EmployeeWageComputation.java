f1b61fa8e98c04bfebc16868a8b2d74e5c0d2ef9
Yash-y-ash <wardhan696@gmail.com>
2021-03-23 14:18:59 +0530

    Added:checking employee attendance

--- src/Emp_Wage_Problem/EmployeeWageComputation.java
+++ src/Emp_Wage_Problem/EmployeeWageComputation.java
@@ -3,8 +3,17 @@ package Emp_Wage_Problem;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to employee wage computation");
+		final int FULL_TIME = 1;
+		int empType = (int) (Math.random() * 100) % 2;
+		if (empType == FULL_TIME)
+		{
+			System.out.println("Employee is Present");
 
+		} else
+		{
+			System.out.println("Employee is Absent");
+
+		}		
 	}
 
 }
