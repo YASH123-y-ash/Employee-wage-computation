f1dd55fa39760becb6c776e037b9e2267dfe8245
Yash-y-ash <wardhan696@gmail.com>
2021-03-25 22:34:53 +0530

    Added:calculating total wage for employee till max hrs or max days is reached

--- src/com/employeewageproblem/EmployeeWageComputation.java
+++ src/com/employeewageproblem/EmployeeWageComputation.java
@@ -1,39 +1,57 @@
 package com.employeewageproblem;
 
+/*
+ calculating total wage of an employee till 
+ maximum working days20 days or max hrs 100hrs is reached
+ using class method.
+ */
 public class EmployeeWageComputation {
 
-	public static void main(String[] args) {
-		final int PART_TIME = 1;
-		final int FULL_TIME = 2;
-		final int WAGE_PER_HR = 20;
-		final int MAX_WORKING_DAYS = 20;
-		final int MAX_WORKING_HRS = 100;
+	// instance variables
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HR = 20;
+	public static final int NUM_WORKING_DAY = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
-		int totalWage = 0;
-		int workingHrs = 0;
-		System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
-		for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-				&& totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
+	//method to get employee wage till max hrs or max working hrs reached
+	public static int computingEmpWage()
+	{
+		//local variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDay = 0;
+
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDay < NUM_WORKING_DAY)
 		{
+			totalWorkingDay++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-			int empType = (int) (Math.random() * 100) % 3;
-			switch (empType)
-			{
-			case FULL_TIME:
-				workingHrs = 8;
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
 				break;
-			case PART_TIME:
-				workingHrs = 4;
+			case IS_FULL_TIME:
+				empHrs = 8;
 				break;
 			default:
-				workingHrs = 0;
-				break;
+				empHrs = 0;
 			}
-			int wage = workingHrs * WAGE_PER_HR;
-			totalWage += wage;
-			System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
+
+			totalEmpHrs += empHrs;
+			System.out.println("Day#:" + totalWorkingDay + " Emp Hr: " + empHrs);
 
 		}
-		System.out.println("Total wage for a month is " + totalWage);
+
+		// total salary of employee
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args)
+	{
+
+		//calling method
+		computingEmpWage();
+
 	}
 }
